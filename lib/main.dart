f9ff045f6e74b44f1ddf73eba15f7c80d69a7662
sonu1509 <sonukr843317@outlook.com>
2022-02-23 22:37:37 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+void main(){
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('DICEE',),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+            child: Image(image:AssetImage('images/dice1.png'))),
+        Expanded(
+            flex: 1,
+            child: Image(image:AssetImage('images/dice1.png')))
+        ],
+    );
+  }
+}
+
